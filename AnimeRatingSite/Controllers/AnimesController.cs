d6a3d36baacf655963909977346b6be7c4cd8ab7
NeonSpider1704 <koby.smith@live.ca>
2022-10-11 15:01:07 -0400

    Added Genre View

---
+++ AnimeRatingSite/Controllers/AnimesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeRatingSite.Controllers
+{
+    public class AnimesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Genre(string GenreName)
+        {
+            return View();
+        }
+    }
+}
